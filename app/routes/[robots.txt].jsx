8fe0de47aa4c736794973896054ada8d3e06a76f
Brandon Lawrence <brandon.lawrence@instacart.com>
2022-09-20 07:52:00 -0700

    seo updates

--- app/routes/[robots.txt].jsx
+++ app/routes/[robots.txt].jsx
@@ -2,13 +2,12 @@ export const loader = () => {
   // handle "GET" request
   // set up our text content that will be returned in the response
   const robotText = `
-      User-agent: Googlebot
-      Disallow: /nogooglebot/
-
       User-agent: *
       Allow: /
 
       Sitemap: http://www.venturacodeand.coffee/sitemap.xml
+
+      ### If you're not a bot, you should attend our meetup https://www.meetup.com/ventura-code-coffee/ ###
       `
   // return the text content, a status 200 success response, and set the content type to text/plain
   return new Response(robotText, {
--- app/routes/[sitemap.xml].jsx
+++ app/routes/[sitemap.xml].jsx
@@ -2,13 +2,21 @@ export const loader = () => {
   // handle "GET" request
   // separating xml content from Response to keep clean code.
   const content = `
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+  <!--
+   created with Free Online Sitemap Generator www.xml-sitemaps.com
+  -->
     <url>
-    <loc>https://www.venturacodeand.coffee/</loc>
-    <lastmod>2022-09-19T00:15:16+01:00</lastmod>
-    <priority>1.0</priority>
+      <loc>https://www.venturacodeand.coffee/</loc>
+      <lastmod>2022-09-20T14:50:19+00:00</lastmod>
+      <priority>1.00</priority>
     </url>
-    </urlset>
+    <url>
+      <loc>https://www.venturacodeand.coffee/conduct</loc>
+      <lastmod>2022-09-20T14:50:19+00:00</lastmod>
+      <priority>0.80</priority>
+    </url>
+  </urlset>
     `
   // Return the response with the content, a status 200 message, and the appropriate headers for an XML page
   return new Response(content, {
