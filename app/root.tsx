76a6018ff73c0d6feac6ac4e74f32dcd5d72acb6
Brandon Lawrence <brandon.lawrence@instacart.com>
2022-10-06 20:41:16 -0700

    more

--- app/root.tsx
+++ app/root.tsx
@@ -147,7 +147,6 @@ function Document({
         <Meta />
         <DynamicLinks />
         <Links />
-        {/* <MetronomeLinks /> */}
       </head>
       <body>
         {children}
@@ -159,19 +158,6 @@ function Document({
             __html: `window.ENV = ${JSON.stringify(data.ENV)}`,
           }}
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(e,t,n,i,s,a,c){e[n]=e[n]||function(){(e[n].q=e[n].q||[]).push(arguments)}
-            ;a=t.createElement(i);c=t.getElementsByTagName(i)[0];a.async=true;a.src=s
-            ;c.parentNode.insertBefore(a,c)
-            })(window,document,"galite","script","https://cdn.jsdelivr.net/npm/ga-lite@2/dist/ga-lite.min.js");
-              window.gtag = galite
-            gtag('create', '${data.ENV.GOOGLE_ANALYTICS_ID}', 'auto');
-            gtag('send', 'pageview');
-        `,
-          }}
-        ></script>
       </body>
     </html>
   )
